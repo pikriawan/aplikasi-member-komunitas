c4b9ac5c81b3d5f157f34ddce1dc6b6b130e1fe6
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-08 14:45:47 +0700

    menambahkan layout dan halaman home

--- resources/js/Pages/Home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 class="m-1 text-5xl font-medium">Laravel</h1>
-</template>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeLayout from '../layouts/HomeLayout.vue';
+</script>
+
+<template>
+    <HomeLayout>
+        <h1 class="m-1 text-5xl font-medium">Laravel</h1>
+    </HomeLayout>
+</template>
