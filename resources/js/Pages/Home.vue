063a1ade9bd922fc7147aaa544cfcbb868ab90c5
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-20 21:53:50 +0700

    menambahkan setting di database seeder

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,18 @@
+<script setup>
+import { usePage } from "@inertiajs/vue3";
+import { computed, watchEffect } from "vue";
+
+const page = usePage();
+
+const setting = computed(() => page.props.setting);
+
+watchEffect(() => {
+    console.log(setting.value);
+});
+</script>
+
 <template>
     <div class="flex flex-col gap-4 p-4">
-        <h1 class="font-semibold text-2xl">Laravel + Vue</h1>
+        <h1 class="font-semibold text-2xl">{{ setting.community_logo || "Logo" }}</h1>
     </div>
 </template>
