e57edec1dfa65c653fa9874c82db18101ebafe78
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-21 22:24:23 +0700

    menambahkan keterangan tidak ada postingan di halaman home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -69,6 +69,7 @@ const posts = computed(() => page.props.posts);
                         </div>
                     </div>
                 </div>
+                <p v-else>Tidak ada postingan.</p>
                 <PillButton :as="Link" :href="route('blog')" variant="outlined">
                     Lihat semua
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
