addfc7d082b28d45ebcbcf402825e362f124de50
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-25 09:18:58 +0700

    redesign halaman login

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,20 +1,44 @@
 <script setup>
-import { Form } from "@inertiajs/vue3";
+import { Form, Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+import PillButton from "../Components/Ui/PillButton.vue";
+import HomeLayout from "../Layouts/HomeLayout.vue";
+
+const passwordVisible = ref(false);
 </script>
 
 <template>
-    <div class="flex flex-col gap-4 p-4">
-        <h1 class="font-semibold text-2xl">Login</h1>
-        <Form class="flex flex-col items-start gap-2" :action="route('login.store')" method="post" v-slot="{ errors }">
-            <div class="flex flex-col gap-1">
-                <input class="ring ring-inset ring-zinc-600 px-2 py-1 rounded-lg" name="email" type="email" placeholder="Email">
-                <p class="text-red-600 text-sm" v-if="errors.email">{{ errors.email }}</p>
+    <HomeLayout>
+        <section class="p-8 lg:p-0 lg:px-4 lg:py-8 lg:flex lg:justify-center bg-primary">
+            <div class="lg:w-full lg:max-w-270 flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4 text-white">
+                <h1 class="font-medium text-2xl">Login</h1>
             </div>
-            <div class="flex flex-col gap-1">
-                <input class="ring ring-inset ring-zinc-600 px-2 py-1 rounded-lg" name="password" type="password" placeholder="Password">
-                <p class="text-red-600 text-sm" v-if="errors.password">{{ errors.password }}</p>
-            </div>
-            <button class="bg-violet-600 text-white px-2 py-1 rounded-lg">Login</button>
-        </Form>
-    </div>
+        </section>
+        <section class="p-8 lg:p-0 lg:px-4 lg:py-16 lg:flex lg:justify-center">
+            <Form class="flex flex-col gap-8 lg:w-full lg:max-w-lg" :action="route('login.store')" method="post" v-slot="{ errors }">
+                <div class="flex flex-col gap-2">
+                    <label class="font-medium" for="email">Email</label>
+                    <input class="w-full px-6 py-2 rounded-full ring ring-inset ring-onyx-400 placeholder:text-onyx-400" name="email" id="email" type="email" placeholder="Email">
+                    <p class="text-danger-500 text-sm" v-if="errors.email">{{ errors.email }}</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <label class="font-medium" for="password">Password</label>
+                    <div class="flex rounded-full ring ring-inset ring-onyx-400">
+                        <input class="w-full px-6 py-2 rounded-l-full placeholder:text-onyx-400" name="password" id="password" :type="passwordVisible ? 'text' : 'password'" placeholder="Password">
+                        <button type="button" class="aspect-square flex justify-center items-center" @click="passwordVisible = !passwordVisible">
+                            <svg v-if="passwordVisible" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
+                        </button>
+                    </div>
+                    <p class="text-danger-500 text-sm" v-if="errors.password">{{ errors.password }}</p>
+                    <Link :href="route('password.request')" class="self-end text-primary">Lupa password?</Link>
+                </div>
+                <PillButton class="justify-center">Login</PillButton>
+                <p class="text-center">
+                    Belum daftar membership?
+                    <Link :href="route('register')" class="text-primary">Daftar membership</Link>
+                </p>
+            </Form>
+        </section>
+    </HomeLayout>
 </template>
