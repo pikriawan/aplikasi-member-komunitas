3bc031041b7d88b13d8fb0e4a2600b52b270bb2e
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-25 13:41:36 +0700

    redesign halaman verifikasi email

--- resources/js/Pages/VerifyEmail.vue
+++ resources/js/Pages/VerifyEmail.vue
@@ -1,20 +1,29 @@
 <script setup>
 import { Form, usePage } from "@inertiajs/vue3";
+import PillButton from "../Components/Ui/PillButton.vue";
+import HomeLayout from "../Layouts/HomeLayout.vue";
 
 const page = usePage();
 </script>
 
 <template>
-    <div class="flex flex-col gap-4 p-4">
-        <h1 class="font-semibold text-2xl">Periksa Email Anda</h1>
-        <Form class="flex flex-col items-start gap-2" :action="route('verification.send')" method="post">
-            <div class="flex flex-col gap-1">
-                <button class="bg-violet-600 text-white px-2 py-1 rounded-lg">Kirim kembali tautan verifikasi</button>
-                <p class="text-lime-500 text-sm" v-if="page.flash.message">{{ page.flash.message }}</p>
+    <HomeLayout>
+        <section class="p-8 lg:p-0 lg:px-4 lg:py-8 lg:flex lg:justify-center bg-primary">
+            <div class="lg:w-full lg:max-w-270 flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4 text-white">
+                <h1 class="font-medium text-2xl">Verifikasi Email</h1>
             </div>
-        </Form>
-        <Form class="flex flex-col items-start gap-2" :action="route('logout')" method="post">
-            <button class="bg-red-600 text-white px-2 py-1 rounded-lg">Logout</button>
-        </Form>
-    </div>
+        </section>
+        <section class="p-8 lg:p-0 lg:px-4 lg:py-16 lg:flex lg:justify-center">
+            <div class="flex flex-col gap-8 lg:w-full lg:max-w-lg">
+                <p>Mohon verifikasi alamat email Anda dengan mengklik tautan yang baru saja kami kirimkan melalui email.</p>
+                <p v-if="page.flash.message" class="text-success-600">{{ page.flash.message }}</p>
+                <Form :action="route('verification.send')" method="post">
+                    <PillButton>Kirim ulang email verifikasi</PillButton>
+                </Form>
+                <Form :action="route('logout')" method="post">
+                    <button class="text-danger-500">Log out</button>
+                </Form>
+            </div>
+        </section>
+    </HomeLayout>
 </template>
