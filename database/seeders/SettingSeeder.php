6442fae063977687b99581908cc9aba86f051616
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-05-30 15:14:02 +0700

    mengubah link eksternal yang mengarah ke akun sosial media tertentu menjadi hashtag

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\Setting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Http\File;
-use Illuminate\Support\Facades\Storage;
 
 class SettingSeeder extends Seeder
 {
@@ -17,28 +15,28 @@ class SettingSeeder extends Seeder
     {
         $settings = [
             'community_name'        => 'Aplikasi Member Komunitas',
-            'community_logo'        => Storage::putFile('', new File(public_path('images/community-logo.svg'))),
+            'community_logo'        => null,
             'email'                 => 'amk@mail.com',
-            'telephone'             => '081200000000',
+            'telephone'             => fake()->phoneNumber(),
             'address'               => 'Gedung Cipta Karya, Jl. Pahlawan Sejahtera No. 5, Surabaya',
-            'x_account_url'         => 'https://x.com/amk',
-            'facebook_account_url'  => 'https://facebook.com/amk',
-            'linkedin_account_url'  => 'https://linkedin.com/amk',
-            'skype_account_url'     => 'https://skype.com/amk',
-            'instagram_account_url' => 'https://instagram.com/amk',
-            'youtube_account_url'   => 'https://youtube.com/amk',
+            'x_account_url'         => null,
+            'facebook_account_url'  => null,
+            'linkedin_account_url'  => null,
+            'skype_account_url'     => null,
+            'instagram_account_url' => null,
+            'youtube_account_url'   => null,
             'account_holder_name'   => 'Aplikasi Member Komunitas',
             'account_number'        => '000000001111',
-            'bank_name'             => 'Bank BRI',
+            'bank_name'             => 'Bank AMK',
             'membership_fee'        => '50000',
             'membership_duration'   => '12',
             'invoice_countdown'     => '24',
             'primary_color'         => '#007FFF',
             'surface_color'         => '#E5F2FF',
-            'background_image'      => Storage::putFile('', new File(public_path('images/background.jpg'))),
+            'background_image'      => null,
             'hero_title'            => 'Bangun Koneksi dan Tumbuh Bersama',
             'hero_description'      => 'Terhubung dengan individu dari berbagai latar belakang, berbagi ide, dan membangun kolaborasi dalam komunitas inklusif untuk berkembang bersama serta menciptakan dampak nyata.',
-            'about_image'           => Storage::putFile('', new File(public_path('images/about.jpg'))),
+            'about_image'           => null,
             'about_title'           => 'Tentang',
             'about_description'     => 'Komunitas ini adalah ruang terbuka bagi siapa saja yang ingin belajar, berkembang, dan saling terhubung dalam lingkungan yang positif dan kolaboratif. Kami menghadirkan berbagai kesempatan untuk bertukar wawasan, membangun relasi, serta berpartisipasi dalam kegiatan yang mendorong pertumbuhan pribadi maupun profesional. Dengan semangat kebersamaan, kami percaya bahwa setiap individu memiliki potensi untuk memberikan kontribusi dan menciptakan dampak yang berarti. Di sini, kamu tidak hanya menjadi bagian dari komunitas, tetapi juga bagian dari perjalanan untuk tumbuh dan berkembang bersama.',
         ];
