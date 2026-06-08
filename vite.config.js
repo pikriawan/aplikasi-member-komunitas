2da2c8ed42af9101fd100e956d893f70081c73ca
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-06-08 17:47:28 +0700

    install ziggy-js

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,4 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
     },
-    resolve: {
-        alias: {
-            'ziggy-js': path.resolve('vendor/tightenco/ziggy'),
-        },
-    },
 });
