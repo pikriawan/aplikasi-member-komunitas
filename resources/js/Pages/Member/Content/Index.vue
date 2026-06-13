eaf66f59b88282bbdef919b9e0af8d4a0ef596b4
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-06-13 08:57:34 +0700

    meletakkan alert di atas filter konten

--- resources/js/Pages/Member/Content/Index.vue
+++ resources/js/Pages/Member/Content/Index.vue
@@ -41,7 +41,10 @@ const links = computed(() => contents.value?.links.map((link) => {
             <div class="flex flex-col gap-8 p-8 lg:min-h-full">
                 <div class="flex-1 flex flex-col gap-8 justify-between">
                     <div class="flex flex-col gap-8">
-                        <div class="flex flex-col gap-8">
+                        <div v-if="memberProfile.is_active" class="flex flex-col gap-8">
+                            <Alert v-for="message in messages" :variant="message.variant" :key="message.text">
+                                {{ message.text }}
+                            </Alert>
                             <div class="flex gap-4">
                                 <Link :class="cn('px-4 py-3 rounded-full bg-surface text-primary', type.value === activeType.value && 'bg-primary text-white')" v-for="[key, type] in ContentType.entries()" :key="key" :href="route('member.contents.index', { _query: { type: type.value } })">
                                     {{ type.label }}
@@ -55,9 +58,6 @@ const links = computed(() => contents.value?.links.map((link) => {
                                 </a>
                             </div>
                         </div>
-                        <Alert v-for="message in messages" :variant="message.variant" :key="message.text">
-                            {{ message.text }}
-                        </Alert>
                     </div>
                     <div class="flex justify-center">
                         <div class="flex justify-center items-center flex-wrap gap-3">
