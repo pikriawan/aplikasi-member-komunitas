7ea0ce96b80ec54da4b4b4ab8a4bd9f8d1ba4839
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-06-28 21:05:44 +0700

    menghapus item blog di panel navigasi di dashboard member, finance, leader, dan super admin

--- resources/js/Layouts/MemberLayout.vue
+++ resources/js/Layouts/MemberLayout.vue
@@ -53,10 +53,6 @@ provide("sidebar", { setSidebarVisible });
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house shrink-0"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
                         Beranda
                     </Link>
-                    <Link class="w-full flex items-center gap-4 p-4 rounded-lg font-medium" :href="route('blog')">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-notebook-icon lucide-notebook shrink-0"><path d="M2 6h4"/><path d="M2 10h4"/><path d="M2 14h4"/><path d="M2 18h4"/><rect width="16" height="20" x="4" y="2" rx="2"/><path d="M16 2v20"/></svg>
-                        Blog
-                    </Link>
                 </nav>
                 <div class="w-full flex flex-col gap-4">
                     <Button :as="Link" v-if="!memberProfile.is_active" class="flex items-center justify-center gap-4 p-4 rounded-lg text-white font-medium bg-primary" :href="route('member.premium.index')">
