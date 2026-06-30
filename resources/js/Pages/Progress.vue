7c2837e893a0cc60dc4192d555f725a3527bb825
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-06-30 16:59:41 +0700

    update progress bar agar sesuai primary color

---
+++ resources/js/Pages/Progress.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { progress } from "@inertiajs/vue3";
+
+function load() {
+    progress.start();
+
+    for (let i = 0; i < 100; i++) {
+        setTimeout(() => {
+            progress.set(i * 0.01);
+        }, i * 100);
+    }
+}
+</script>
+
+<template>
+    <button @click="load">Load</button>
+</template>
